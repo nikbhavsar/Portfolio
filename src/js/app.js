46020de6dfe13590afcb66b0fc73aa2ab8f6f57a
Nikhar <bhavsarnikhar21@gmail.com>
2019-10-24 01:07:20 -0700

    Added contact section and some javascript functionality
    Added contact section
    Added social media section for mobile
    Added javascript functionality for navbar

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,28 @@ import '../css/style.scss';
 
 /********** Paste your code here! ************/
 
-console.log('Paste your code here!');
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
+
+// show and hide navbar
+
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+
+   
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".nav-bar").style.top = "0";
+    } else {
+        document.querySelector(".nav-bar").style.top = "-100px";
+    }
+
+   
+    prevScrollpos = currentScrollPos;
+}

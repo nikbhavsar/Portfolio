6f21d7347a5afdbac3649b8231df2d6d9a6e3f28
Nikhar <bhavsarnikhar21@gmail.com>
2019-10-24 21:14:24 -0700

    Finish Final touchup
    Added last section
    Updated css to be compatible with safari

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,7 @@
 import '../css/style.scss';
 
 
-/********** Paste your code here! ************/
-
+// Toggle Navbar 
 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -18,18 +17,53 @@ navBarToggle.addEventListener('click', function () {
 
 // show and hide navbar
 
+if (window.pageYOffset === 0) {
+    document.querySelector(".nav-bar").classList.add("top-nav");
+} else {
+    document.querySelector(".nav-bar").classList.remove("top-nav");
+}
+
 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
 
-   
-    if (prevScrollpos > currentScrollPos) {
+    if (currentScrollPos === 0) {
+        document.querySelector(".nav-bar").classList.add("top-nav");
+    } else if (prevScrollpos > currentScrollPos) {
         document.querySelector(".nav-bar").style.top = "0";
+        document.querySelector(".nav-bar").classList.remove("top-nav");
+
     } else {
         document.querySelector(".nav-bar").style.top = "-100px";
+        document.querySelector(".nav-bar").classList.remove("top-nav");
     }
 
-   
     prevScrollpos = currentScrollPos;
 }
+
+
+//Typing text
+
+let charArray = "web and mobile.".split("");
+let spanElement = document.querySelector(".typing-text");
+
+let i = 0;
+let txt = 'Web and Mobile'; /* The text */
+let speed = 250; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.querySelector(".typing-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else if (i === txt.length) {
+        i = 0;
+        document.querySelector(".typing-text").innerHTML = "";
+        setTimeout(typeWriter, 1000);
+
+    }
+}
+
+document.onload(typeWriter());
+
